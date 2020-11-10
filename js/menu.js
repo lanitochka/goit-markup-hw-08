1d9b9b24a06e98552e1e08dc197e161f4c4b01f5
lanitochka <70783335+lanitochka@users.noreply.github.com>
2020-11-10 23:31:36 +0100

    quick update

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,7 @@
     const mobileMenuRef = document.querySelector("[data-menu]");
   
     menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
   
       menuBtnRef.classList.toggle("is-open");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
